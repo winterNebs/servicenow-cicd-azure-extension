0b6cd029880be68095924d185b3dcd19f9da6513
winternebs <winternebs@gmail.com>
2024-12-13 12:06:25 -0500

    Update build

--- build.js
+++ build.js
@@ -1,45 +1,58 @@
-const fs = require('fs');
-const fse = require('fs-extra');
-const archiver = require('archiver');
-const path = require('path');
-const {execSync} = require('child_process');
+const fs = require("fs");
+const fse = require("fs-extra");
+const archiver = require("archiver");
+const path = require("path");
+const { execSync } = require("child_process");
 
 const tasks = [
-    'AppInstall',
-    'AppPublish',
-    'AppRollback',
-    'PluginActivate',
-    'PluginRollback',
-    'SCApply',
-    'TestRun',
-    'ScanInstance',
-    'BatchInstall'
+    "AppInstall",
+    "AppPublish",
+    "AppRollback",
+    "PluginActivate",
+    "PluginRollback",
+    "SCApply",
+    "TestRun",
+    "ScanInstance",
+    "BatchInstall"
 ];
 
 function copy(src, dst) {
     return fse.copy(path.join(__dirname, src), path.join(__dirname, dst));
 }
 
-function getVersion(task = '') {
-    if ('' === task) {
+function getVersion(task = "") {
+    if ("" === task) {
         return new Promise((resolve, reject) => {
-            fs.readFile(path.join(__dirname, 'src/extension/extension.vsixmanifest'), (err, body) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    let match = body.toString().match(/<Identity [^>]+Version="(\d+\.\d+\.\d+)"/);
-                    if (!match) {
-                        reject('Could not get version from extension.vsixmanifest');
+            fs.readFile(
+                path.join(__dirname, "src/extension/extension.vsixmanifest"),
+                (err, body) => {
+                    if (err) {
+                        reject(err);
                     } else {
-                        resolve(match[1].split('.').map(d => +d));
+                        let match = body
+                            .toString()
+                            .match(/<Identity [^>]+Version="(\d+\.\d+\.\d+)"/);
+                        if (!match) {
+                            reject(
+                                "Could not get version from extension.vsixmanifest"
+                            );
+                        } else {
+                            resolve(match[1].split(".").map((d) => +d));
+                        }
                     }
                 }
-            });
+            );
         });
     } else {
         return fse
-            .readJson(path.join(__dirname, 'src/extension/Tasks', task, 'task.json'))
-            .then(json => [+json.version.Major, +json.version.Minor, +json.version.Patch]);
+            .readJson(
+                path.join(__dirname, "src/extension/Tasks", task, "task.json")
+            )
+            .then((json) => [
+                +json.version.Major,
+                +json.version.Minor,
+                +json.version.Patch
+            ]);
     }
 }
 
@@ -49,110 +62,158 @@ function getVersion(task = '') {
  * @param version Array
  */
 function setVersion(version) {
-    const
-        stringVersion = version.join('.'),
+    const stringVersion = version.join("."),
         objVersion = {
             Major: version[0],
             Minor: version[1],
             Patch: version[2]
         };
-    let promises = tasks.map(task => {
-        const fName = path.join(__dirname, 'src/extension/Tasks', task, 'task.json');
-        return fse
-            .readJson(fName)
-            .then(json => {
-                json.version = objVersion;
-                return fse.writeJson(fName, json, {spaces: 4});
-            });
-    });
-    promises.push(new Promise((resolve, reject) => {
-        const fName = path.join(__dirname, 'src/extension/extension.vsixmanifest');
-        fs.readFile(fName, (err, body) => {
-            if (err) {
-                reject(err);
-            } else {
-                body = body
-                    .toString()
-                    .replace(
-                        /(<Identity [^>]+Version=")(\d+\.\d+\.\d+)"/,
-                        '$1' + stringVersion + '"'
-                    );
-                fs.writeFile(fName, body, err => err ? reject(err) : resolve());
-            }
+    let promises = tasks.map((task) => {
+        const fName = path.join(
+            __dirname,
+            "src/extension/Tasks",
+            task,
+            "task.json"
+        );
+        return fse.readJson(fName).then((json) => {
+            json.version = objVersion;
+            return fse.writeJson(fName, json, { spaces: 4 });
         });
-    }));
+    });
+    promises.push(
+        new Promise((resolve, reject) => {
+            const fName = path.join(
+                __dirname,
+                "src/extension/extension.vsixmanifest"
+            );
+            fs.readFile(fName, (err, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    body = body
+                        .toString()
+                        .replace(
+                            /(<Identity [^>]+Version=")(\d+\.\d+\.\d+)"/,
+                            "$1" + stringVersion + '"'
+                        );
+                    fs.writeFile(fName, body, (err) =>
+                        err ? reject(err) : resolve()
+                    );
+                }
+            });
+        })
+    );
     return Promise.all(promises).then(() => stringVersion);
 }
 
 function updateVersion() {
-    return Promise
-        .all(['', ...tasks].map(task => getVersion(task)))
-        .then(versions => {
+    return Promise.all(["", ...tasks].map((task) => getVersion(task))).then(
+        (versions) => {
             let version = versions.pop();
-            versions.forEach(v => {
+            versions.forEach((v) => {
                 if (
                     v[0] > version[0] ||
                     (v[0] === version[0] && v[1] > version[1]) ||
-                    (v[0] === version[0] && v[1] === version[1] && v[2] > version[2])
+                    (v[0] === version[0] &&
+                        v[1] === version[1] &&
+                        v[2] > version[2])
                 ) {
                     version = v;
                 }
             });
             // version[2]++;
             return setVersion(version);
-        });
+        }
+    );
 }
 
 function build(version) {
-    const outDir = path.join(__dirname, 'out');
-    return fse.ensureDir(path.join(__dirname, 'tmp'), 0o2775)
+    const outDir = path.join(__dirname, "out");
+    return fse
+        .ensureDir(path.join(__dirname, "tmp"), 0o2775)
         .then(() => fse.emptyDir(outDir))
-        .then(() => copy('src/extension', 'out'))
-        .then(() => Promise.all(tasks.map(task => Promise.all([
-                copy('src/lib/AzureDevopsPipeline.js', `out/Tasks/${task}/AzureDevopsPipeline.js`),
-                copy('src/lib/ServiceNowCICDRestAPIService.js', `out/Tasks/${task}/ServiceNowCICDRestAPIService.js`),
-                copy('src/lib/index.js', `out/Tasks/${task}/index.js`),
-                copy(`src/lib/${task}.js`, `out/Tasks/${task}/task.js`),
-                copy('prod_deps/node_modules', `out/Tasks/${task}/node_modules`)
-            ])))
+        .then(() => copy("src/extension", "out"))
+        .then(() =>
+            Promise.all(
+                tasks.map((task) =>
+                    Promise.all([
+                        copy(
+                            "src/lib/AzureDevopsPipeline.js",
+                            `out/Tasks/${task}/AzureDevopsPipeline.js`
+                        ),
+                        copy(
+                            "src/lib/ServiceNowCICDRestAPIService.js",
+                            `out/Tasks/${task}/ServiceNowCICDRestAPIService.js`
+                        ),
+                        copy("src/lib/index.js", `out/Tasks/${task}/index.js`),
+                        copy(`src/lib/${task}.js`, `out/Tasks/${task}/task.js`),
+                        copy(
+                            "prod_deps/node_modules",
+                            `out/Tasks/${task}/node_modules`
+                        )
+                    ])
+                )
+            )
         )
         .then(() => createArchive(version))
-        .then(() => fse.move(path.join(__dirname, 'tmp/servicenow.vsix'), path.join(__dirname, `out/servicenow.extension.${version}.vsix`)))
-        .then(() => console.log(`##vso[task.setvariable variable=artifactName;isOutput=true]servicenow.extension.${version}.vsix`));
+        .then(() =>
+            fse.move(
+                path.join(__dirname, "tmp/servicenow.vsix"),
+                path.join(__dirname, `out/servicenow.extension.${version}.vsix`)
+            )
+        )
+        .then(() =>
+            console.log(
+                `##vso[task.setvariable variable=artifactName;isOutput=true]servicenow.extension.${version}.vsix`
+            )
+        );
 }
 
 function createArchive() {
-    console.log('Creating archive');
+    console.log("Creating archive");
     return new Promise((resolve, reject) => {
-        const output = fs.createWriteStream(path.join(__dirname, `tmp/servicenow.vsix`));
-        const archive = archiver('zip', {
-            zlib: {level: 5} // Sets the compression level.
+        const output = fs.createWriteStream(
+            path.join(__dirname, `tmp/servicenow.vsix`)
+        );
+        const archive = archiver("zip", {
+            zlib: { level: 5 } // Sets the compression level.
         });
-        output.on('close', () => resolve());
-        output.on('end', () => resolve());
+        output.on("close", () => resolve());
+        output.on("end", () => resolve());
 
-        archive.on('warning', err => reject(err));
-        archive.on('error', err => reject(err));
-        archive.directory(path.join(__dirname, 'out'), false);
+        archive.on("warning", (err) => reject(err));
+        archive.on("error", (err) => reject(err));
+        archive.directory(path.join(__dirname, "out"), false);
         archive.pipe(output);
         archive.finalize();
     });
 }
-function installDependencies(){
-    const outDir = path.join(__dirname, 'prod_deps');
+// TODO: This should be done using a buildtool to bundle the node modules. This is a hacky work around...
+function installDependencies() {
+    const outDir = path.join(__dirname, "prod_deps");
     console.log("copying package.json to ", outDir);
     // copy package.json, run the prod build so we can copy it to extension
-   return fse.copy(path.join(__dirname, "package.json"),path.join(outDir,"package.json")).then(()=>{
-       new Promise(()=>{
-        console.log("installing packages");
-        execSync("npm install --production --prune --prefix " + outDir);
-        console.log("done installing packages");
-       })
-    }).catch(err=>console.log(err));
+    fse.removeSync(outDir);
+    return fse
+        .copy(
+            path.join(__dirname, "package.json"),
+            path.join(outDir, "package.json")
+        )
+        .then(() => {
+            new Promise(() => {
+                console.log("installing packages");
+                //execSync("npm install --production --prune --prefix " + outDir);
+                execSync(
+                    "cd " + outDir + " && npm install --production --prune"
+                );
+                console.log("done installing packages");
+            });
+        })
+        .catch((err) => console.log(err));
 }
-installDependencies().then(()=>{
-updateVersion()
-    .then(version => build(version))
-    .then(() => console.log('success'))
-    .catch(e => console.error(e))
+installDependencies().then(() => {
+    updateVersion()
+        .then((version) => build(version))
+        .then(() => console.log("success"))
+        .catch((e) => console.error(e));
 });
